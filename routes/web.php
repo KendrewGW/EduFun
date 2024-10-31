eb29b13518387f8cbbf73d54a64746d4fe527a62
KendrewGW <kendrewgiantoriwidjaja@gmail.com>
2024-10-31 21:08:40 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::prefix('category', function () {
+    Route::get('/datascience', function () {
+        return view('category.datascience');
+    });
+
+    Route::get('/networksecurity', function () {
+        return view('category.networksecurity');
+    });
+});
+
+Route::get('/writers', function () {
+    return view('writers');
+});
+
+Route::get('/aboutus', function () {
+    return view('aboutus');
+});
+
+Route::get('/popular', function () {
+    return view('aboutus');
+});
